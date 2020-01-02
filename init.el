54485f8fe77264c711cf5da636bb80db59d32073
Pengji Zhang <me@pengjiz.com>
2020-01-02 10:38:14 -0500

    Tweak key bindings

--- init.el
+++ init.el
@@ -1431,7 +1431,7 @@ This is a non-interactive version of `ignore'."
 (use-package counsel-extras
   :load-path "lisp"
   :after counsel
-  :bind ("C-c f f" . counsel-extras-fd)
+  :bind ("C-c f j" . counsel-extras-fd)
   :config (counsel-extras-setup))
 
 (use-package amx
