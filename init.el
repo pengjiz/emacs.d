fb4e3907b05a61fded5aa32f32bb98b0df92018e
Pengji Zhang <me@pengjiz.com>
2021-04-09 19:08:55 -0400

    Tweak initial bookmarks

--- init.el
+++ init.el
@@ -713,8 +713,7 @@
   :init (setf bookmark-default-file (my-expand-var-file-name "bookmarks"))
   :config
   (unless (file-exists-p bookmark-default-file)
-    (dolist (item '(("finances" . "ledger/finances.ledger")
-                    ("bookcase" . "bookcase/inventory.org")))
+    (dolist (item '(("finances" . "ledger/finances.ledger")))
       (let ((filename (my-expand-sync-file-name (cdr item))))
         (when (file-exists-p filename)
           (cl-pushnew `(,(car item) . ((filename . ,filename))) bookmark-alist
