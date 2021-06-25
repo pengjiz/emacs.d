6ea1e20518c33429a1ea82f35ecaf245010f9c28
Pengji Zhang <me@pengjiz.com>
2021-06-25 19:09:23 -0400

    Improve erc-extras

--- lisp/erc-extras.el
+++ lisp/erc-extras.el
@@ -8,60 +8,35 @@
 
 (require 'erc)
 (require 'erc-services)
-(eval-when-compile
-  (require 'subr-x))
 
 ;;; NickServ identification
 
-(defvar erc-extras--nickserv-read-password erc-prompt-for-nickserv-password
-  "Whether to ask for NickServ password.")
+(defun erc-extras--search-nickserv-password (nickname)
+  "Search NickServ password for NICKNAME."
+  (or (and erc-nickserv-passwords
+           (cdr (assoc nickname
+                       (cadr (assq (erc-network)
+                                   erc-nickserv-passwords)))))
+      (let ((server (erc-with-server-buffer erc-session-server)))
+        (auth-source-pick-first-password :host server
+                                         :port "nickserv"
+                                         :user nickname
+                                         :require '(:secret)))))
 
-(defun erc-extras--search-nickserv-auth-source-password (nickname)
-  "Search NickServ password for NICKNAME with auth-source."
-  (when-let* ((entry (car (auth-source-search :max 1
-                                              :host erc-session-server
-                                              :port "nickserv"
-                                              :user nickname
-                                              :require '(:secret))))
-              (secret (plist-get entry :secret)))
-    (if (functionp secret)
-        (funcall secret)
-      secret)))
-
-(defun erc-extras--search-nickserv-variable-password (nickname)
-  "Search NickServ password for NICKNAME in `erc-nickserv-passwords'."
-  (when erc-nickserv-passwords
-    (cdr (assoc nickname
-                (cadr (assq (erc-network) erc-nickserv-passwords))))))
-
-(defun erc-extras--call-nickserv-identify-function (nickname)
-  "Call function to identify NICKNAME to NickServ."
-  (let ((password (and erc-extras--nickserv-read-password
-                       (read-passwd (format "NickServ password for %s on %s: "
-                                            nickname
-                                            (or (erc-network)
-                                                "unknown network"))))))
-    (when (or (not password) (string-empty-p password))
-      (setf password
-            (or (erc-extras--search-nickserv-variable-password nickname)
-                (erc-extras--search-nickserv-auth-source-password nickname))))
-    (erc-nickserv-identify password)))
-
-;; NOTE: By default ERC will not identify nickname if both variables for
-;; prompting password and nickserv passwords are nil. Here we locally bind it to
-;; t to force identification.
+;; HACK: ERC will not identify to NickServ if the both variables for passwords
+;; are nil. So here we use a dummy entry to force identification because we also
+;; query auth-source for passwords.
 (defun erc-extras--force-nickserv-identification (fn &rest args)
   "Apply FN on ARGS, but force NickServ identification."
-  (let ((erc-extras--nickserv-read-password erc-prompt-for-nickserv-password)
-        (erc-prompt-for-nickserv-password t))
+  (let ((erc-nickserv-passwords (or erc-nickserv-passwords '((dummy)))))
     (apply fn args)))
 
 ;;; Entry point
 
 (defun erc-extras-setup ()
   "Setup ERC extensions."
-  (setf (symbol-function 'erc-nickserv-call-identify-function)
-        #'erc-extras--call-nickserv-identify-function)
+  (setf (symbol-function 'erc-nickserv-get-password)
+        #'erc-extras--search-nickserv-password)
   (advice-add #'erc-nickserv-identify-autodetect :around
               #'erc-extras--force-nickserv-identification)
   (advice-add #'erc-nickserv-identify-on-connect :around
