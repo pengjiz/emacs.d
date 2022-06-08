e498fb1d0e9ed02a7ed4fdb94ce1a2f7a67eedbf
Pengji Zhang <me@pengjiz.com>
2022-06-08 16:47:38 -0400

    Tweak package archives

--- init.el
+++ init.el
@@ -119,6 +119,7 @@
 (progn ; package
   (require 'package)
   (setf package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                           ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                            ("melpa" . "https://melpa.org/packages/")))
 
   (defun init--record-selected-package ()
