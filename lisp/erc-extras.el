acd5b74f79115af99c1da41bd6516de1658cee9d
Pengji Zhang <me@pengjiz.com>
2022-10-19 16:49:13 -0400

    Improve erc-extras

--- lisp/erc-extras.el
+++ lisp/erc-extras.el
@@ -8,8 +8,17 @@
 
 (require 'erc)
 (require 'erc-services)
+(require 'erc-desktop-notifications)
 (eval-when-compile
   (require 'subr-x))
+
+;;; Option
+
+(defcustom erc-extras-hide-notification-messages
+  t
+  "Whether to hide messages in desktop notifications."
+  :type 'boolean
+  :group 'erc-notifications)
 
 ;;; NickServ identification
 
@@ -36,12 +45,26 @@
               (read-passwd (format "NickServ password for %s on %s: "
                                    nickname network)))))))
 
+;;; Hide notification messages
+
+(defun erc-extras--adjust-notification-message (args)
+  "Adjust notification message appropriately based on ARGS."
+  (let ((nickname (nth 0 args))
+        (private (nth 2 args)))
+    (cond ((and erc-extras-hide-notification-messages private)
+           (list nickname "Sent you a message" private))
+          (erc-extras-hide-notification-messages
+           (list nickname "Mentioned you" private))
+          (t args))))
+
 ;;; Entry point
 
 (defun erc-extras-setup ()
   "Setup ERC extensions."
   (setf (symbol-function 'erc-nickserv-get-password)
-        #'erc-extras--get-nickserv-password))
+        #'erc-extras--get-nickserv-password)
+  (advice-add 'erc-notifications-notify :filter-args
+              #'erc-extras--adjust-notification-message))
 
 (provide 'erc-extras)
 ;;; erc-extras.el ends here
