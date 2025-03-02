e41bc34214f5899361fc2e8805df79e4a6d62ebb
Pengji Zhang <me@pengjiz.com>
2025-03-02 19:45:14 +0800

    Prioritize official package archives

--- init.el
+++ init.el
@@ -123,7 +123,9 @@
   (require 'package)
   (setf package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                            ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-                           ("melpa" . "https://melpa.org/packages/")))
+                           ("melpa" . "https://melpa.org/packages/"))
+        package-archive-priorities '(("gnu" . 10)
+                                     ("nongnu" . 5)))
 
   (defun init--record-selected-packages ()
     "Record selected packages if any."
